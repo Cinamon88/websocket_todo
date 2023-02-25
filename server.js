448de6740832be09a65b04c1043cecb4c480867a
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-25 16:46:02 +0100

    add build and fix for replit

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
-
-const tasks = [];
-
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+const server = app.listen(8000, () => {
+    console.log('Server is running on Port:', 8000)
 });
 
 const path = require('path');
 const io = socket(server);
 
+const tasks = [];
+
+
 app.use(express.static(path.join(__dirname, '/client/')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/public/index.html'));
-});
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+    res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
 io.on("connection", (socket) => {
