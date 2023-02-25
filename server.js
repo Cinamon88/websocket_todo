7ec5d8c676e524db559067f9ab7676d3f354d54c
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-25 16:30:36 +0100

    fix path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socket(server);
 app.use(express.static(path.join(__dirname, '/client/')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/index.html'));
+    res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
 app.use((req, res) => {
